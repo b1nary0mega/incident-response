2c2c2e2a6adffd3aee51b0a6d096dbbd0de0ff34
Jimmi <b1nary0mega@users.noreply.github.com>
2019-08-26 14:40:00 -0400

    reodering of functions
    reodering of functions

--- JCERT.ps1
+++ JCERT.ps1
@@ -26,6 +26,12 @@ Data Gathering:
 [ ] Network Information & Connections
 
 #>
+# Get Machine Autoruns
+function autorunsQuery {
+    Write-host "...pulling autorun info..." -foregroundcolor green 
+    .\autorunsc64.exe -accepteula -a * -c > ($dumpFileName + "\" + $env:ComputerName + "-autoruns.csv")
+    $fileNames.Add($dumpFileName + "\" + $env:ComputerName + "-autoruns.csv")
+}
 
 # Get Machine Information (SOURCE: http://www.sans.org/sec505)
 function computerInfo {
@@ -34,102 +40,12 @@ function computerInfo {
     $fileNames.Add($dumpFileName + "\" + $env:ComputerName +  "-ComputerInfo.txt")
 }
 
-# Pull Machine Logs
-function logCopy {
-    $Logs = @{Security = "Security"; Application = "Application"; System = "System"; Powershell = "Microsoft-Windows-PowerShell/Operational"}
-    Write-host "...copying log files..." -foregroundcolor green 
-    forEach ($log in $Logs.keys) {
-        Write-host "... ... " $log "... ..." -foregroundcolor blue 
-        wevtutil epl $Logs[$log] ($dumpFileName + "\logs\" + $env:ComputerName + "-" + $log + ".evtx")
-        $fileNames.Add($dumpFileName + "\logs\" + $env:ComputerName + "-" + $log + ".evtx") 
-    }
-}
-
-# Get Prefetch files
-function prefetchCopy {
-    Write-host "...copying prefetch files..." -foregroundcolor green 
-    Copy-Item c:\windows\prefetch\*.pf ($dumpFileName + "\prefetch") -recurse
-
-    <#TODO##
-        Hash all items & store output
-    #>
-}
-
-# Get Machine Autoruns
-function autorunsQuery {
-    Write-host "...pulling autorun info..." -foregroundcolor green 
-    .\autorunsc64.exe -accepteula -a * -c > ($dumpFileName + "\" + $env:ComputerName + "-autoruns.csv")
-    $fileNames.Add($dumpFileName + "\" + $env:ComputerName + "-autoruns.csv")
-}
-
-# Get Machine Hives (must have run script with elevated privs)
-function hiveCopy {
-    $Hives = "SYSTEM","SOFTWARE","SAM"
-    Write-host "...pulling registry hive(s)..." -foregroundcolor green 
-    forEach ($hive in $Hives){
-        Write-host "... ... " $hive "... ..." -foregroundcolor blue 
-        reg save HKLM\SYSTEM ($dumpFileName + "\hives\" + $env:ComputerName + "-" + $hive)
-        $fileNames.Add($dumpFileName + "\hives\" + $env:ComputerName + "-" + $hive)
-    }
-}
-
-# Recreate a PSTREE function for this windows instance
-# SOURCE: Adam Roben @ https://gist.github.com/aroben/5542538
-function processTreeExport {
-    Write-host "...pulling process tree..." -foregroundcolor green
-    $fileNames.Add($dumpFileName + "\" + $env:ComputerName + "-ProcessTree.txt")
-    if(1) {
-        $ProcessesById = @{}
-
-        foreach ($Process in (Get-WMIObject -Class Win32_Process)) {
-          $ProcessesById[$Process.ProcessId] = $Process
-        }
-
-        $ProcessesWithoutParents = @()
-        $ProcessesByParent = @{}
-        foreach ($Pair in $ProcessesById.GetEnumerator()) {
-          $Process = $Pair.Value
-
-          if (($Process.ParentProcessId -eq 0) -or !$ProcessesById.ContainsKey($Process.ParentProcessId)) {
-            $ProcessesWithoutParents += $Process
-            continue
-          }
-
-          if (!$ProcessesByParent.ContainsKey($Process.ParentProcessId)) {
-            $ProcessesByParent[$Process.ParentProcessId] = @()
-          }
-          $Siblings = $ProcessesByParent[$Process.ParentProcessId]
-          $Siblings += $Process
-          $ProcessesByParent[$Process.ParentProcessId] = $Siblings
-        }
-
-        function Show-ProcessTree([UInt32]$ProcessId, $IndentLevel) {
-          $Process = $ProcessesById[$ProcessId]
-      
-          $Indent = ("." * $IndentLevel)
-          if ($IndentLevel -eq 0){
-            $Indent =""
-          }
-          $commandline = $Process.CommandLine
-          $Name  = $Indent + $Process.Name
-          $ExePath = $Process.Executablepath
-          $owner = $Process.getowner()
-          $user = $owner.domain + "\"  + $owner.user
-          $creationdate_str = $Process.ConvertToDateTime($Process.CreationDate)
-
-          Write-Output ("{0,6} {1,-30} {2,-30} {3} {4} {5}" -f $Process.ProcessId, $Name, $user, $creationdate_str, $ExePath, $commandline)
-          foreach ($Child in ($ProcessesByParent[$ProcessId] | Sort-Object CreationDate)) {
-            Show-ProcessTree $Child.ProcessId ($IndentLevel + 2)
-          }
-        }
-
-        Write-Output ("{0,6} {1} {2} {3} {4}" -f "PID", "Name                          ", "User                          ","Creation date      ","Image path") | out-file -Append -encoding ASCII -filepath ($dumpFileName + "\" + $env:ComputerName + "-ProcessTree.txt")
-        Write-Output ("{0,6} {1} {2} {3} {4}" -f "---", "----------------------------  ","----------------------------  ","-------------------","-----------") | out-file -Append -encoding ASCII -filepath ($dumpFileName + "\" + $env:ComputerName + "-ProcessTree.txt")
-
-        foreach ($Process in ($ProcessesWithoutParents | Sort-Object CreationDate)) {
-          Write-Output (Show-ProcessTree $Process.ProcessId 0) | out-file -Append -encoding ASCII -filepath ($dumpFileName + "\" + $env:ComputerName + "-ProcessTree.txt")
-        }
-    }
+# Hash all files
+function fileHasher {
+    Write-host "...hashing all the things..." -foregroundcolor green 
+    ForEach ($file in $fileNames) {
+        get-filehash ($file) | format-list | out-file -append ($dumpFileName + "--HASHES.txt")
+    } 
 }
 
 # SOURCE: Boe Prox @ https://mcpmag.com/articles/2015/06/18/reporting-on-local-groups.aspx
@@ -197,12 +113,95 @@ function GroupQuery {
   }
 }
 
-# Hash all files
-function fileHasher {
-    Write-host "...hashing all the things..." -foregroundcolor green 
-    ForEach ($file in $fileNames) {
-        get-filehash ($file) | format-list | out-file -append ($dumpFileName + "--HASHES.txt")
-    } 
+# Get Machine Hives (must have run script with elevated privs)
+function hiveCopy {
+    $Hives = "SYSTEM","SOFTWARE","SAM"
+    Write-host "...pulling registry hive(s)..." -foregroundcolor green 
+    forEach ($hive in $Hives){
+        Write-host "... ... " $hive "... ..." -foregroundcolor blue 
+        reg save HKLM\SYSTEM ($dumpFileName + "\hives\" + $env:ComputerName + "-" + $hive)
+        $fileNames.Add($dumpFileName + "\hives\" + $env:ComputerName + "-" + $hive)
+    }
+}
+
+# Pull Machine Logs
+function logCopy {
+    $Logs = @{Security = "Security"; Application = "Application"; System = "System"; Powershell = "Microsoft-Windows-PowerShell/Operational"}
+    Write-host "...copying log files..." -foregroundcolor green 
+    forEach ($log in $Logs.keys) {
+        Write-host "... ... " $log "... ..." -foregroundcolor blue 
+        wevtutil epl $Logs[$log] ($dumpFileName + "\logs\" + $env:ComputerName + "-" + $log + ".evtx")
+        $fileNames.Add($dumpFileName + "\logs\" + $env:ComputerName + "-" + $log + ".evtx") 
+    }
+}
+
+# Get Prefetch files
+function prefetchCopy {
+    Write-host "...copying prefetch files..." -foregroundcolor green 
+    Copy-Item c:\windows\prefetch\*.pf ($dumpFileName + "\prefetch") -recurse
+
+    <#TODO##
+        Hash all items & store output
+    #>
+}
+
+# Recreate a PSTREE function for this windows instance
+# SOURCE: Adam Roben @ https://gist.github.com/aroben/5542538
+function processTreeExport {
+    Write-host "...pulling process tree..." -foregroundcolor green
+    $fileNames.Add($dumpFileName + "\" + $env:ComputerName + "-ProcessTree.txt")
+    if(1) {
+        $ProcessesById = @{}
+
+        foreach ($Process in (Get-WMIObject -Class Win32_Process)) {
+          $ProcessesById[$Process.ProcessId] = $Process
+        }
+
+        $ProcessesWithoutParents = @()
+        $ProcessesByParent = @{}
+        foreach ($Pair in $ProcessesById.GetEnumerator()) {
+          $Process = $Pair.Value
+
+          if (($Process.ParentProcessId -eq 0) -or !$ProcessesById.ContainsKey($Process.ParentProcessId)) {
+            $ProcessesWithoutParents += $Process
+            continue
+          }
+
+          if (!$ProcessesByParent.ContainsKey($Process.ParentProcessId)) {
+            $ProcessesByParent[$Process.ParentProcessId] = @()
+          }
+          $Siblings = $ProcessesByParent[$Process.ParentProcessId]
+          $Siblings += $Process
+          $ProcessesByParent[$Process.ParentProcessId] = $Siblings
+        }
+
+        function Show-ProcessTree([UInt32]$ProcessId, $IndentLevel) {
+          $Process = $ProcessesById[$ProcessId]
+      
+          $Indent = ("." * $IndentLevel)
+          if ($IndentLevel -eq 0){
+            $Indent =""
+          }
+          $commandline = $Process.CommandLine
+          $Name  = $Indent + $Process.Name
+          $ExePath = $Process.Executablepath
+          $owner = $Process.getowner()
+          $user = $owner.domain + "\"  + $owner.user
+          $creationdate_str = $Process.ConvertToDateTime($Process.CreationDate)
+
+          Write-Output ("{0,6} {1,-30} {2,-30} {3} {4} {5}" -f $Process.ProcessId, $Name, $user, $creationdate_str, $ExePath, $commandline)
+          foreach ($Child in ($ProcessesByParent[$ProcessId] | Sort-Object CreationDate)) {
+            Show-ProcessTree $Child.ProcessId ($IndentLevel + 2)
+          }
+        }
+
+        Write-Output ("{0,6} {1} {2} {3} {4}" -f "PID", "Name                          ", "User                          ","Creation date      ","Image path") | out-file -Append -encoding ASCII -filepath ($dumpFileName + "\" + $env:ComputerName + "-ProcessTree.txt")
+        Write-Output ("{0,6} {1} {2} {3} {4}" -f "---", "----------------------------  ","----------------------------  ","-------------------","-----------") | out-file -Append -encoding ASCII -filepath ($dumpFileName + "\" + $env:ComputerName + "-ProcessTree.txt")
+
+        foreach ($Process in ($ProcessesWithoutParents | Sort-Object CreationDate)) {
+          Write-Output (Show-ProcessTree $Process.ProcessId 0) | out-file -Append -encoding ASCII -filepath ($dumpFileName + "\" + $env:ComputerName + "-ProcessTree.txt")
+        }
+    }
 }
 
 <###########################################################
@@ -227,12 +226,37 @@ forEach ($dest in $destinations) {
 
 #all your datas belong to $dumpFileName ;)
 computerInfo
-logCopy
+
+### Enumerate Files ###
 prefetchCopy
-autorunsQuery
+logCopy
 hiveCopy
-ProcessTreeExport
+
+### Collect network caches ###
+
+### Collect Users ###
 GroupQuery -Computername  $env:COMPUTERNAME -Group  Administrators,  Users  | Format-List 
+
+### Analyze Startup Items ###
+autorunsQuery
+
+### Analyze Programs Run ###
+### Collect Network Shares ###
+### Collect System Configuration ###
+### Analyze Scheduled tasks ###
+
+### Analyze event logs ###
+logCopy
+
+### Collect Processes ###
+ProcessTreeExport
+
+### Collect Network Connections and Ports ###
+### Collect web files ###
+### Analyze all files ###
+
+#finish up by providing a hash for all pulled data and files
 fileHasher
 
+#let the user know we are finished
 Write-host "[+] Completed Data Acquisition" -foregroundcolor green 
